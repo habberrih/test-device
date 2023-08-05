f5b63468ed6d56a9e46c30062e131de6df6c333c
Abdullah Habberrih <abdul2013ly@gmail.com>
2023-08-05 15:59:28 +0200

    editing

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-
 
 // Middlewares
 app.use(express.json());
 
-app.post('/api', (req, res) => {
-    const data = req.body;
+app.route("/api").get((req, res) => {
+  const data = req.body;
 
-    JSON.parse(data)
-    return res.status(200).json(data);
+  res.send(data);
 });
-
-
-app.get('/api', (req, res) => {
-    const data = req.body;
-
-    JSON.parse(data)
-    return res.status(200).json('Hello');
-});
-
-
 module.exports = app;
